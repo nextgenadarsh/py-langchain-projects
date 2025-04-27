bd99fc21cb3e867e6ae34ea9b8b023cf2c9ac449
Adarsh Kumar <nextgen.adarsh@outlook.com>
2025-04-27 11:39:57 +0530

    Cleanup

--- 1_langchain_agent_basic/main.py
+++ 1_langchain_agent_basic/main.py
@@ -1,6 +1,6 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
 from dotenv import load_dotenv
